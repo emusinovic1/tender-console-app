06e2bd6f0b6bed55f2983c2cd39c57d93b8de460
maidabiber <mbiber1@etf.unsa.ba>
2025-12-25 00:43:16 +0100

    vracena stara verzija main funkcije u Program.cs

--- VVS_TenderApp/VVS_TenderApp/Program.cs
+++ VVS_TenderApp/VVS_TenderApp/Program.cs
@@ -21,7 +21,7 @@ namespace VVS_TenderApp
 
         static Korisnik ulogovanKorisnik = null;
 
-        /*  static void Main(string[] args)
+          static void Main(string[] args)
             {
                 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
@@ -54,53 +54,9 @@ namespace VVS_TenderApp
                     }
                 }
             }
-       */
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
+       
 
-            // --- DIO ZA BENCHMARK TESTIRANJE ---
-            Console.Clear();
-            Console.WriteLine(">>> POKREĆEM FINALNI BENCHMARK TEST (PERFORMANCE & METRICS) <<<");
-            Console.WriteLine("Molimo sačekajte par sekundi...");
-
-            // Pozivamo tvoj tuning kod sa novim metrikama (CC i MI)
-            VVS_TenderApp.Tuning.ValidirajIPosaljiPonuduTuning.Run();
-
-            Console.WriteLine("\nTestiranje završeno. Pritisnite bilo koji taster za ulazak u program...");
-            Console.ReadKey();
-            // -----------------------------------
-
-            Console.Clear();
-            Console.WriteLine("--SISTEM ZA UPRAVLJANJE TENDERIMA--");
-            Console.WriteLine();
-            Console.WriteLine("Pritisnite bilo koji taster za nastavak...");
-            Console.ReadKey();
-
-            while (true)
-            {
-                try
-                {
-                    if (ulogovanKorisnik == null)
-                    {
-                        PocetniMeni();
-                    }
-                    else
-                    {
-                        if (ulogovanKorisnik.Uloga == Uloga.Admin)
-                            AdministratorMeni();
-                        else
-                            FirmaMeni();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"\nNeočekivana greška: {ex.Message}");
-                    Console.WriteLine("Pritisnite bilo koji taster za nastavak...");
-                    Console.ReadKey();
-                }
-            }
-        }
+ 
 
 
         static void PocetniMeni()
